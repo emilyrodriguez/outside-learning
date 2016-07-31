7eb0110f62eae690ed17511126e0ced96dbde951
Emily A Rodriguez <rodriguez.emily1993@gmail.com>
2016-07-31 13:31:42 -0500

    Hello world file

---
+++ java/helloworld.java
@@ -0,0 +1,12 @@
+public class HelloWorld 
+{
+	static String randomString = "String to print";
+
+	static final double PINUM = 3.14159;
+	public static void main(String args)
+	{
+		// System.out.println("Hello World!");
+		System.out.println(PINUM);
+
+	}
+}
