b4511192f3e36f36a6e387789d5becffca907239
Emily A Rodriguez <rodriguez.emily1993@gmail.com>
2016-08-08 15:09:42 -0500

    basic hello world exercise

---
+++ python/helloworld.py
@@ -0,0 +1,12 @@
+# Print and Strings
+name = "Emily"
+print(name)
+print(name+" says 'Hello World!'")
+print("Concatenate"+" these words!")
+print("5 + 2 =", 5+2)
+print("5 - 2 =", 5-2)
+print("5 * 2 =", 5*2)
+print("5 / 2 =", 5/2)
+print("5 % 2 =", 5%2)
+print("5 ** 2 =", 5**2)
+print("5 // 2 =", 5//2)
