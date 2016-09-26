18cc15c0c444920144f018178808d63e5c6d0335
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-09-26 13:51:51 -0500

    using prop types to recieve data

--- React JS/js/Note.js
+++ React JS/js/Note.js
@@ -6,8 +6,8 @@ var Note = React.createClass({
         this.setState({editing: true});
     },
     save: function() {
-    	var val = this.refs.newText.getDOMNode().value;
-    	alert("TODO: Save note value: " + val);
+        var val = this.refs.newText.getDOMNode().value;
+        alert("TODO: Save note value" + val);
         this.setState({editing: false});
     },
     remove: function() {
@@ -45,5 +45,33 @@ var Note = React.createClass({
     }
 });
 
-React.render(<Note>Hello World</Note>, 
+var Board = React.createClass({
+    propTypes: {
+        count: function(props, propName) {
+            if (typeof props[propName] !== "number"){
+                return new Error('The count property must be a number');
+            }
+            if (props[propName] > 100) {
+                return new Error("Creating " + props[propName] + " notes is ridiculous");
+            }
+        }
+    },
+
+    render: function() {
+        return <div className="board">{this.props.count}</div>
+    }
+});
+
+
+React.render(<Board count={10}/>, 
     document.getElementById('react-container'));
+
+
+
+
+
+
+
+
+
+
