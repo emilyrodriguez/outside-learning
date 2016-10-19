866a6724a1bb690d2de01fcfca0c12166c313563
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-10-19 11:15:48 -0500

    Added golf challenge

--- jsChallenges/jsChallenges.js
+++ jsChallenges/jsChallenges.js
@@ -6,3 +6,23 @@ function convertToF(celsius) {
 }
 
 convertToF(0);
+
+// figure out golf score
+function golfScore(par, strokes) {
+  if (strokes == 1) {
+    return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+    return "Eagle";
+  } else if (strokes == par - 1 ) {
+    return "Birdie";
+  } else if (strokes == par) {
+    return "Par";
+  } else if (strokes == par + 1) {
+    return "Bogey";
+  } else if (strokes == par + 2) {
+    return "Double Bogey";
+  } else if (strokes >= par + 3) {
+    return "Go Home!";
+  }
+}
+golfScore(5, 4);
