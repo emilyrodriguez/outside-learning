e68b81047fed749842d37ae6ffd544e7f9de5edd
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-10-19 11:58:47 -0500

    Added card counter

--- jsChallenges/jsChallenges.js
+++ jsChallenges/jsChallenges.js
@@ -1,28 +1,49 @@
 // convert celcius to fahrenheit
 function convertToF(celsius) {
-  var fahrenheit;
-  fahrenheit = celsius * 9/5 + 32;
-  return fahrenheit;
+	var fahrenheit;
+	fahrenheit = celsius * 9/5 + 32;
+	return fahrenheit;
 }
 
 convertToF(0);
 
 // figure out golf score
 function golfScore(par, strokes) {
-  if (strokes == 1) {
-    return "Hole-in-one!";
-  } else if (strokes <= par - 2) {
-    return "Eagle";
-  } else if (strokes == par - 1 ) {
-    return "Birdie";
-  } else if (strokes == par) {
-    return "Par";
-  } else if (strokes == par + 1) {
-    return "Bogey";
-  } else if (strokes == par + 2) {
-    return "Double Bogey";
-  } else if (strokes >= par + 3) {
-    return "Go Home!";
-  }
+	if (strokes == 1) {
+		return "Hole-in-one!";
+	} else if (strokes <= par - 2) {
+		return "Eagle";
+	} else if (strokes == par - 1 ) {
+		return "Birdie";
+	} else if (strokes == par) {
+		return "Par";
+	} else if (strokes == par + 1) {
+		return "Bogey";
+	} else if (strokes == par + 2) {
+		return "Double Bogey";
+	} else if (strokes >= par + 3) {
+		return "Go Home!";
+	}
 }
 golfScore(5, 4);
+
+// blackjack card counter
+var count = 0;
+
+function cc(card) {
+	if (card <= 6) {
+	  count++;
+	} else if (card <= 9) {
+	  count += 0;
+	} else {
+	  count--;
+	}
+	if (count <= 0){
+	  return count + " Hold"; 
+	} else {
+	  return count + " Bet"; 
+	}
+
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
